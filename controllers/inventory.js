df9119198e04c323d96b5c0ddd97daff06f21ff4
olalekan temitayo <olalekantemitayo7@gmail.com>
2022-05-14 17:11:23 +0100

    added sqlite

--- controllers/inventory.js
+++ controllers/inventory.js
@@ -10,12 +10,12 @@ const createItem = async (req, res) => {
     body: { name, quantity },
   } = req
 
-  if ((quantity === '') || (!quantity)) {
+  if (!quantity) {
     throw new BadRequestError('Quantity is required')
   }
 
-  if (name === '' || (!name)) {
-    throw new BadRequestError('Name are required')
+  if (!name) {
+    throw new BadRequestError('Name is required')
   }
 
   const created_item = await prisma.item.create({ data: req.body })
@@ -28,7 +28,7 @@ const updateItem = async (req, res) => {
   const name = req.body.name
   const quantity = req.body.quantity
 
-  if (name === '' || quantity === '') {
+  if ((!name) || (!quantity)) {
     throw new BadRequestError('fields are required')
 
   }
@@ -69,24 +69,28 @@ const getItem = async (req, res) => {
 const deleteItem = async (req, res) => {
   const id = req.params.id
   const comment = req.body.comment
-  if (comment === '' || (!comment)) {
+  if (!comment) {
     throw new BadRequestError('Comment is required for deletion')
   }
 
   const item = await prisma.item.findFirst({
     where: {
       id: Number(id),
-    }
-  })
+      deleted: false
+    },
 
-  if (item.deleted) {
-    throw new BadRequestError('Item has been deleted')
+  })
+  if (!item) {
+    throw new BadRequestError('Item already deleted')
   }
+
+ 
 
   const updated_item = await prisma.item.update({
     where: { id: Number(id) },
     data: { deleted: true, comment: comment },
   })
+  // res.status(StatusCodes.OK).json({ message: "Item deleted", data: { name: _item.name, comment: _item.comment } })
   res.status(StatusCodes.OK).json({ message: "Item deleted", data: { name: updated_item.name, comment: updated_item.comment } })
 
 }
